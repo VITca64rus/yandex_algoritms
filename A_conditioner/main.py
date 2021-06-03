ef6730beb879fee1d78d43df6fb96a80ce7f3e53
Podlevskiy Viktor <vitca64rus@gmail.com>
2021-06-03 07:52:57 +0300

    a_conditions

---
+++ A_conditioner/main.py
@@ -0,0 +1,23 @@
+with open("input.txt", "r") as file:
+    lines = file.readlines()
+
+troom = int(lines[0].split()[0])
+tcond = int(lines[0].split()[1])
+work = lines[1].split()[0]
+
+print(work)
+print(tcond, troom)
+
+
+if (work == 'freeze') and (tcond < troom):
+    troom = tcond
+
+if (work == 'heat') and (tcond > troom):
+    troom = tcond
+
+if work == 'auto':
+    troom = tcond
+
+
+with open("output.txt", "w") as file:
+    file.write(str(troom))
