3e1409feae76fc94a4505e96d7037908b1bbf838
Podlevskiy Viktor <vitca64rus@gmail.com>
2021-06-05 13:37:45 +0300

    it's good for E

--- home_work_2/E_chemp/main.py
+++ home_work_2/E_chemp/main.py
@@ -5,6 +5,7 @@ for i in range(0, n):
     numbers[i] = int(numbers[i])
 
 pobed = max(numbers)
+#print('Бросок победителя - ',pobed)
 
 def get_id_pobed(numbers,n):
     res=[]
@@ -14,14 +15,19 @@ def get_id_pobed(numbers,n):
     return min(res)
 
 id_pobed = get_id_pobed(numbers,n)
+#print('ИД победителя - ',id_pobed)
 
 
 def get_meters_vasy(numbers,id_pobed, n):
-    result = 1001
-    for i in range(1, n-1):
-        if (str(numbers[i])[-1:] == '5') and (numbers[i+1] < numbers[i]) and (i < id_pobed):
-            result = numbers[i]
-    return result
+    result = []
+    for i in range(id_pobed+1, n-1):
+
+        if (str(numbers[i])[-1:] == '5') and (numbers[i+1] < numbers[i]):
+            result.append(numbers[i])
+    if result:
+        return max(result)
+    else:
+        return 1001
 
 
 def get_pos(n, numbers, vas_res):
@@ -30,7 +36,8 @@ def get_pos(n, numbers, vas_res):
             return i+1
 
 
-vas_res = get_meters_vasy(numbers, pobed, n)
+vas_res = get_meters_vasy(numbers, id_pobed, n)
+#print(vas_res)
 if vas_res != 1001:
     numbers.sort(reverse=True)
     print(get_pos(n, numbers, vas_res))
